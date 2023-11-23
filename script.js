2dee23878a179825d4a694a90e2c08b2d0373bd4
Roshan <roshan.16n@gmail.com>
2023-11-23 15:27:28 +0530

    Dark-light mode

---
+++ script.js
@@ -0,0 +1,18 @@
+var mode = 0
+
+function toggle() {
+    if (mode == 0) {
+        mode = 1;
+        document.getElementById("button").style.translate = "40px"
+        document.getElementById("button").style.background = "black"
+        document.getElementById("toggle").style.background = "white"
+        document.getElementById("toggle").style.border = "8px solid white"
+        document.body.style.background = "black"
+    } else {
+        mode = 0;document.getElementById("button").style.translate = "0px"
+        document.getElementById("button").style.background = "white"
+        document.getElementById("toggle").style.background = "black"
+        document.getElementById("toggle").style.border = "8px solid black"
+        document.body.style.background = "white"
+    }
+}
