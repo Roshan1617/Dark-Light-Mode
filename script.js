b0b8ce19ca7fe9dbd553083b1c849c62f401115a
Roshan <roshan.16n@gmail.com>
2023-11-23 23:29:12 +0530

    added light and dark icon with posititon absolute

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ var mode = 0
 function toggle() {
     if (mode == 0) {
         mode = 1;
-        document.getElementById("button").style.translate = "40px"
+        document.getElementById("button").style.translate = "20px"
         document.getElementById("button").style.background = "black"
         document.getElementById("toggle").style.background = "white"
-        document.getElementById("toggle").style.border = "8px solid white"
+        document.getElementById("toggle").style.border = "2px solid white"
         document.body.style.background = "black"
     } else {
         mode = 0;document.getElementById("button").style.translate = "0px"
-        document.getElementById("button").style.background = "white"
-        document.getElementById("toggle").style.background = "black"
-        document.getElementById("toggle").style.border = "8px solid black"
+        document.getElementById("button").style.background = "black"
+        document.getElementById("toggle").style.background = "white"
+        document.getElementById("toggle").style.border = "2px solid black"
         document.body.style.background = "white"
     }
 }
